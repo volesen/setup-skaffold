406ab66f50e8b9ffd481fa48c2726bd7a04a1f30
volesen <vincolesen@gmail.com>
2020-11-24 17:03:14 +0100

    Added unit test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,4 +3,12 @@ import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
-test('Throws on window platform', async () => {})
+let os = {} as any
+
+beforeAll(() => {
+  os.platform = 'aix'
+})
+
+test('Throws on unsupported platforms', async () => {
+  expect(getSkaffoldDownloadInfo('latest')).resolves.toThrow
+})
